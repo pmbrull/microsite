418b9e70951475d88c2c08fea16335382fa1e8c8
pmbrull <peremiquelbrull@gmail.com>
2019-12-07 18:55:52 +0100

    copy style in run

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 sbt clean
 sbt makeMicrosite
+# light-style.css is missing, so we copy it
+mkdir target/site/css
+cp docs/resources/styles/light-style.css target/site/css/
 cd target/site/
-echo $pwd
 jekyll serve -b /microsite
